1583e27d33242b77de1c557fba3fd94b9030424e
sanjay <kumarsanjay60986@gmail.com>
2026-08-27 16:25:11 +0530

    added oneline

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ def generate_password(length=12):
     characters = string.ascii_letters + string.digits + string.punctuation
     return ''.join(random.choice(characters) for i in range(length))
 
-print("Generated pass:", generate_password(15))
+print("Generated password:", generate_password(15))
